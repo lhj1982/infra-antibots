4717c195c7cbad2d9798e6d5f8d1e3cf69d83756
weiZhong <Wei.Zhong2@nike.com>
2023-11-20 14:11:52 +0800

    Initial commit

---
+++ test/infra-antibots.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as InfraAntibots from '../lib/infra-antibots-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/infra-antibots-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new InfraAntibots.InfraAntibotsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
